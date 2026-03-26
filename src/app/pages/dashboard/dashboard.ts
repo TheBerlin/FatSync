82de217417656572dff75f199ee9f4f9b7b650db
Bohdan <bsamischenko@gmail.com>
2026-03-26 13:14:19 +0100

    style: Dashboard rework

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Supabase } from '../../services/supabase';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule],
   standalone: true,
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
+  public supabase = inject(Supabase);
+  
   isPaid: boolean = false;
   user = {name: "Alex"};
 
