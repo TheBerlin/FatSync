60eae162b1dfef35767c471b709f230f997ff0aa
Bohdan <bsamischenko@gmail.com>
2026-03-28 19:17:46 +0100

    style: added general icons style

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { Button } from '../button/button';
 import { CommonModule } from '@angular/common';
+import { HugeiconsIconComponent } from '@hugeicons/angular';
+import { PlayIcon } from '@hugeicons/core-free-icons';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [Button, CommonModule],
+  imports: [Button, CommonModule, HugeiconsIconComponent],
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
 export class Hero {
+  // Icon
+  PlayIcon = PlayIcon;
+
+  // Modal
   isModalOpen = false;
 
   openModal() {
--- src/app/components/problem/problem.ts
+++ src/app/components/problem/problem.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { HugeiconsIconComponent } from '@hugeicons/angular';
+import {
+  ArrowDataTransferDiagonalIcon,
+  TimeQuarter02Icon,
+  User03Icon,
+} from '@hugeicons/core-free-icons';
 
 @Component({
   selector: 'app-problem',
+  imports: [HugeiconsIconComponent],
   standalone: true,
   templateUrl: './problem.html',
   styleUrl: './problem.css',
 })
-export class Problem {}
+export class Problem {
+  ArrowIcon = ArrowDataTransferDiagonalIcon;
+  TimeIcon = TimeQuarter02Icon;
+  UserIcon = User03Icon;
+}
