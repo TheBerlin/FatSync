0329ea25738ce9af503c058b3b28ef000b21fd2c
Bohdan <bsamischenko@gmail.com>
2026-03-29 19:02:42 +0200

    design: added side menu for mobile devices

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -3,14 +3,23 @@ import { Button } from '../button/button';
 import { RouterLink } from '@angular/router';
 import { Login } from '../../pages/login/login';
 import { Supabase } from '../../services/supabase';
+import { HugeiconsIconComponent } from '@hugeicons/angular';
+import { Cancel01Icon, Menu01Icon } from '@hugeicons/core-free-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [Button, RouterLink, Login],
+  imports: [Button, RouterLink, Login, HugeiconsIconComponent],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  MenuIcon = Menu01Icon;
+  CloseIcon = Cancel01Icon;
   public supabase = inject(Supabase);
+
+  isMenuOpen = false;
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
