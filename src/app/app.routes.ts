3ca882623d56a6ed35b314d29b7246a7e4ac8bc7
Bohdan <bsamischenko@gmail.com>
2026-03-29 19:23:14 +0200

    Revert "design: fix button & icon placement"
    This reverts commit 3a84eec1f07dc844ccc9b0aea5fc112901a898cc.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,18 +6,14 @@ import { LandingComponent } from './components/landing-component/landing-compone
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Login } from './pages/login/login';
 import { authGuard } from './auth-guard';
-import { DashboardTest } from './pages/dashboard-test/dashboard-test';
+
 
 export const routes: Routes = [
-  { path: 'dashboard-test', component: DashboardTest },
-
-  { path: '', component: LandingComponent },
-  { path: 'login', component: Login, pathMatch: 'full' },
-  { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
-
-  { path: 'privacy', component: LegalPrivacy },
-  { path: 'terms', component: LegalTerms },
-  { path: 'support', component: Support },
-
-  { path: '**', redirectTo: '' },
+    { path: '', component: LandingComponent },
+    { path: 'login', component: Login, pathMatch: 'full' },
+    { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
+    { path: 'privacy', component: LegalPrivacy },
+    { path: 'terms', component: LegalTerms },
+    { path: 'support', component: Support },
+    { path: '**', redirectTo: ''},
 ];
